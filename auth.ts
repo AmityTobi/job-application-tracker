592223a77d92a0d3c6adfb98f4f33cba464cec7e
amitytobi <amityekoyi@gmail.com>
2026-09-04 00:03:02 +0100

    configure NextAuth with GitHub provider and Prisma adapter

---
+++ auth.ts
@@ -0,0 +1,22 @@
+import NextAuth from "next-auth";
+import Github from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "@/lib/db";
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  adapter: PrismaAdapter(db),
+  providers: [Github],
+  callbacks: {
+    async session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+});
