17295c41b5ea9a830b55e51f490dd8c1bae5d6ab
amitytobi <amityekoyi@gmail.com>
2026-09-11 13:20:35 +0100

    add login and logout server action wrappers

---
+++ actions/index.ts
@@ -0,0 +1,5 @@
+export { loginAction } from "@/actions/login";
+export { logoutAction } from "@/actions/logout";
+export { createApplicationAction } from "@/actions/createApplication";
+export { updateApplicationAction } from "@/actions/updateApplication";
+export { deleteApplicationAction } from "@/actions/deleteApplication";
